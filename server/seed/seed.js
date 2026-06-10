d1bfe6de49233e0ce3b48a959f6b954a6ac1706e
NAVEENKUMAR-T-GIT-28 <naveenkumarsnr28@gmail.com>
2026-06-10 13:05:06 +0530

    dns added for mongodb

--- server/seed/seed.js
+++ server/seed/seed.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const dns = require('dns');
+
+// Force Node.js to use public DNS servers (Google and Cloudflare) to resolve MongoDB SRV records
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 const User = require('../models/User');
 const Category = require('../models/Category');
 
