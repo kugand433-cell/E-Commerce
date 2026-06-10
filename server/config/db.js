494895cdd28a892aa76d0ce8d4ab28d199557acf
NAVEENKUMAR-T-GIT-28 <naveenkumarsnr28@gmail.com>
2026-06-10 12:57:05 +0530

    dns issue solved for mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Force Node.js to use public DNS servers (Google and Cloudflare) to resolve MongoDB SRV records
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const connectDB = async () => {
   try {
